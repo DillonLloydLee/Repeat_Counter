15be7ef4ef0aa706afe1645608b648d46a780504
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 12:45:18 -0700

    add response for blank inputs.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,42 +5,49 @@
 
             $output = 0;
 
-            $search_stripped = str_replace(";", "", $search);
-            $search_stripped = str_replace(":", "", $search_stripped);
-            $search_stripped = str_replace(".", "", $search_stripped);
-            $search_stripped = str_replace("?", "", $search_stripped);
-            $search_stripped = str_replace("!", "", $search_stripped);
-            $search_stripped = str_replace('"', "", $search_stripped);
-            $search_stripped = str_replace("-", "", $search_stripped);
-            $search_stripped = str_replace(",", "", $search_stripped);
+            if ($text != "" || $search != ""){
 
-            $search_word = strtolower($search_stripped);
-            $search_word_unstripped = strtolower($search);
+                $search_stripped = str_replace(";", "", $search);
+                $search_stripped = str_replace(":", "", $search_stripped);
+                $search_stripped = str_replace(".", "", $search_stripped);
+                $search_stripped = str_replace("?", "", $search_stripped);
+                $search_stripped = str_replace("!", "", $search_stripped);
+                $search_stripped = str_replace('"', "", $search_stripped);
+                $search_stripped = str_replace("-", "", $search_stripped);
+                $search_stripped = str_replace(",", "", $search_stripped);
 
-            $text_stripped = str_replace(";", "", $text);
-            $text_stripped = str_replace(":", "", $text_stripped);
-            $text_stripped = str_replace(".", "", $text_stripped);
-            $text_stripped = str_replace("?", "", $text_stripped);
-            $text_stripped = str_replace("!", "", $text_stripped);
-            $text_stripped = str_replace('"', "", $text_stripped);
-            $text_stripped = str_replace("-", "", $text_stripped);
-            $text_stripped = str_replace(",", "", $text_stripped);
+                $search_word = strtolower($search_stripped);
+                $search_word_unstripped = strtolower($search);
 
-            $text_to_search = explode(" ", strtolower($text_stripped));
-            $text_to_search_unstripped = explode(" ", strtolower($text));
+                $text_stripped = str_replace(";", "", $text);
+                $text_stripped = str_replace(":", "", $text_stripped);
+                $text_stripped = str_replace(".", "", $text_stripped);
+                $text_stripped = str_replace("?", "", $text_stripped);
+                $text_stripped = str_replace("!", "", $text_stripped);
+                $text_stripped = str_replace('"', "", $text_stripped);
+                $text_stripped = str_replace("-", "", $text_stripped);
+                $text_stripped = str_replace(",", "", $text_stripped);
+
+                $text_to_search = explode(" ", strtolower($text_stripped));
+                $text_to_search_unstripped = explode(" ", strtolower($text));
 
 
-            foreach ($text_to_search as $possible_word) {
-                if ($possible_word == $search_word) {
-                    $output += 1;
-                }
-            }
-            if ($output == 0) {
-                foreach ($text_to_search_unstripped as $possible_word) {
-                    if ($possible_word == $search_word_unstripped) {
+                foreach ($text_to_search as $possible_word) {
+                    if ($possible_word == $search_word) {
                         $output += 1;
                     }
                 }
+                if ($output == 0) {
+                    foreach ($text_to_search_unstripped as $possible_word) {
+                        if ($possible_word == $search_word_unstripped) {
+                            $output += 1;
+                        }
+                    }
+                }
+            }
+            else {
+                $search = "NOTHING";
+                $output = "IMPOSSIBLE TO COMPUTE!";
             }
 
             $output = array($search, $output);
