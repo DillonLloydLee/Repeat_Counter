080e6919834dea8404b8266f32b0abc48cab0ffc
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 12:17:37 -0700

    Write routes, especially root route.

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/XXXXXXX.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
@@ -11,7 +11,7 @@
 
     $app->get('/results', function() use ($app) {
         $this_RepeatCounter = new RepeatCounter;
-        $this_number = $this_RepeatCounter->countRepeats($_GET['word']);
+        $this_number = $this_RepeatCounter->countRepeats($_GET["text"], $_GET["search"]);
         return $app['twig']->render('results.twig', array("data" => $this_number));
     });
 
