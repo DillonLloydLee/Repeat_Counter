c160b23b788bcb6e5346782b853d50052d13c7a0
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 10:23:47 -0700

    Fifth test: add preg_replace to function, test pass.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,7 +5,8 @@
 
             $output = 0;
             $search_word = strtolower($search);
-            $text_to_search = explode(" ", strtolower($text));
+            $text_stripped = preg_replace("/[^a-z0-9]+/i", " ", $text);
+            $text_to_search = explode(" ", strtolower($text_stripped));
 
             foreach ($text_to_search as $possible_word) {
                 if ($possible_word == $search_word) {
