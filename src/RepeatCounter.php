acfb698d0732b63e819235b7b8c96a146fe64f0e
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 10:06:44 -0700

    Fourth test: edit class and pass.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,7 +5,7 @@
 
             $output = 0;
             $search_word = strtolower($search);
-            $text_to_search = str_split(strtolower($text));
+            $text_to_search = explode(" ", strtolower($text));
 
             foreach ($text_to_search as $possible_word) {
                 if ($possible_word == $search_word) {
