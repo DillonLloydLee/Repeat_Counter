ec39af4328a4ba9c6a6fa4e2eed9337e4e6643d8
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 10:04:30 -0700

    Fourth test: write and fail.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,7 +4,7 @@
     class RepeatCounterTest extends PHPUnit_Framework_TestCase {
 
         //Test One: a single letter in a single letter.
-        function test_countRepeats_singleLetter() {
+        function test_countRepeats_one() {
 
             $test_RepeatCounter = new RepeatCounter;
             $text = "a";
@@ -16,7 +16,7 @@
         }
 
         //Test Two: a single letter amongst several letters.
-        function test_countRepeats_singleLetterInSeveral() {
+        function test_countRepeats_two() {
 
             $test_RepeatCounter = new RepeatCounter;
             $text = "a big dog";
@@ -28,11 +28,23 @@
         }
 
         //Test Three: a capitalized letter.
-        function test_countRepeats_singleCapitalized() {
+        function test_countRepeats_three() {
 
             $test_RepeatCounter = new RepeatCounter;
             $text = "A big dog is a good dog.";
             $search = "a";
+
+            $result = $test_RepeatCounter->countRepeats($text, $search);
+
+            $this->assertEquals(2, $result);
+        }
+
+        //Test Four: ignore other word inclusion of search word.
+        function test_countRepeats_four() {
+
+            $test_RepeatCounter = new RepeatCounter;
+            $text = "The big dog named Theodore is the thief.";
+            $search = "the";
 
             $result = $test_RepeatCounter->countRepeats($text, $search);
 
