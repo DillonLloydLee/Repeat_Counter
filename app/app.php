ffbf195df0105fef4b081013df06aaf11445e6f3
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 09:10:36 -0700

    Initialize git repository.  Add README.md and some basic files.

---
+++ app/app.php
@@ -0,0 +1,13 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/XXXXXXX.php";
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('root.twig');
+    });
+
+    return $app;
+ ?>
