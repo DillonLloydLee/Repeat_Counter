2a03825dd21e9e50311633169aeca36738de3649
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-14 09:57:46 -0700

    Second Test: edit class and pass.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,9 +4,12 @@
         function countRepeats($text, $search) {
 
             $output = 0;
+            $explode_text = str_split($text);
 
-            if ($text == $search) {
-                $output += 1;
+            foreach ($explode_text as $possible_word) {
+                if ($possible_word == $search) {
+                    $output += 1;
+                }
             }
 
             return $output;
